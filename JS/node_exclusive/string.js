b2f3d49e542f514e0d8e1c8ffc4f608a6e3bb6c6
Parthib Sarkar <119702740+parthibsid5@users.noreply.github.com>
2023-12-27 13:03:20 +0530

    Added string tutorial

---
+++ JS/node_exclusive/string.js
@@ -0,0 +1,10 @@
+console.log("This is string tutorial..")
+let a = "Samsung"
+console.log(a[4])
+console.log(a.slice(0))
+console.log(a.slice(0, 5))
+let fname = "Parthib"
+let lname = "Sarkar"
+console.log("My first name is " + fname + " and last name is " + lname)
+
+console.log(`My first name is ${fname} and last name is ${lname}`)
